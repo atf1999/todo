efbd6a7125ea505d798cd5bbbf4ed884a3ed3d00
atf1999 <torth212@gmail.com>
2015-08-03 20:05:12 -0400

    Basic script done

--- todo.sh
+++ todo.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
 function help(){
- echo "param"
+ echo "COMMANDS: "
+ echo "        --help: List commands"
+ echo "        -init : Pass LISTNAME along with this command to make file"
+ echo "        -a: Pass LISTNAME and LISTITEM to add items. If ROWNUM is not empty, adds text to line at number specified"
 }
-function newfile(){
-touch $file
-}
-file=${TODO:=$HOME/todo.txt}
-
+file=${TODO:=$HOME}
 
 if  [[ $1 = "--help" ]]; then
     help
-elif [[ $1 = "-init" ]]; then
-    newfile
-elif [[ $1 = "-n" ]] && [[ $2 != "" ]]; then
-    $2 >> $file  
+elif [[ $1 = "-init" ]] && [[ $2 != "" ]]; then
+    touch $file/$2 + ".txt"
+elif [[ $1 = "-a" ]] && [[ $2 != "" ]] && [[ $3 != "" ]] && [[ $4 = "" ]]; then
+    final=$file/$2.txt
+    echo $3 >> $final
+elif [[ $1 = "-a" ]] && [[ $2 != "" ]] && [[ $3 != "" ]] && [[ $4 != "" ]]; then
+    final=$file/$2.txt
+    s=$4
+    s+="i"
+    s+=\
+    s+=$3
+    sed -i $s $final
 fi
 
 
